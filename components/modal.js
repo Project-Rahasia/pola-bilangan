de2ba0f0de6eac4287dab3b931769e16ea6769e5
rkhaidir <rkhaidir085@gmail.com>
2022-05-01 06:19:52 +0800

    add page teacher, fix bug

--- components/modal.js
+++ components/modal.js
@@ -9,49 +9,146 @@ document.querySelector(".modal").innerHTML = `
       <!-- List course -->
       <div class="accordion accordion-flush" id="accordionFlushExample">
         <div class="accordion-item">
-        <h2 class="accordion-header" id="flush-headingOne">
-          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-            Accordion Item #1
-          </button>
-        </h2>
-        <div id="flush-collapseOne" class="accordion-collapse collapse show" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+          <h2 class="accordion-header" id="flush-headingOne">
+            <button class="accordion-button ${getCollapsed(
+              "sub-1"
+            )}" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+              Pendahuluan
+            </button>
+          </h2>
+        <div id="flush-collapseOne" class="accordion-collapse collapse ${getChapClass(
+          "sub-1"
+        )}" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
           <div class="accordion-body">
             <div class="list-course mb-3">
-              <a href="/pages/materi-1/0/" class="list-course-child">Pendahuluan</a>
+              <a href="/materi/sub-1/0/" class="list-course-child ${getChapChild(
+                "sub-1/0/"
+              )}">Kompetensi Dasar</a>
             </div>
             <div class="list-course mb-3">
-              <a href="/pages/materi-1/1/" class="list-course-child">Titik Pusat</a>
+              <a href="/materi/sub-1/1/" class="list-course-child ${getChapChild(
+                "sub-1/1/"
+              )}">Peta Konsep</a>
+            </div>
+            <div class="list-course mb-3">
+              <a href="/materi/sub-1/2/" class="list-course-child ${getChapChild(
+                "sub-1/2/"
+              )}">Pendahuluan</a>
             </div>
           </div>
         </div>
       </div>
       <div class="accordion-item">
         <h2 class="accordion-header" id="flush-headingTwo">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-            Accordion Item #2
+          <button class="accordion-button ${getCollapsed(
+            "sub-2"
+          )}" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+            Menentukan Persamaan dari Suatu Barisan Bilangan
           </button>
         </h2>
-        <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-          <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.
+        <div id="flush-collapseTwo" class="accordion-collapse collapse ${getChapClass(
+          "sub-2"
+        )}" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+          <div class="accordion-body">
+            <div class="list-course mb-3">
+            <a href="/materi/sub-2/0/" class="list-course-child ${getChapChild(
+              "sub-2/0/"
+            )}">Pendahuluan</a>
+          </div>
+          <div class="list-course mb-3">
+            <a href="/materi/sub-2/1/" class="list-course-child ${getChapChild(
+              "sub-2/1/"
+            )}">Barisan Bilangan Ganjil dan Genap</a>
+          </div>
+          <div class="list-course mb-3">
+            <a href="/materi/sub-2/2/" class="list-course-child ${getChapChild(
+              "sub-2/2/"
+            )}">Barisan Bilangan Fibonacci</a>
+          </div>
+          <div class="list-course mb-3">
+            <a href="/materi/sub-2/3/" class="list-course-child ${getChapChild(
+              "sub-2/3/"
+            )}">Pola Bilangan Segitiga Pascal</a>
+          </div>
+          <div class="list-course mb-3">
+            <a href="/materi/sub-2/4/" class="list-course-child ${getChapChild(
+              "sub-2/4/"
+            )}">Barisan Bilangan Lainnya</a>
+          </div>
+          <div class="list-course mb-3">
+            <a href="/materi/sub-2/5/" class="list-course-child ${getChapChild(
+              "sub-2/5/"
+            )}">Kuis</a>
+          </div>
           </div>
         </div>
       </div>
       <div class="accordion-item">
         <h2 class="accordion-header" id="flush-headingThree">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-            Accordion Item #3
+          <button class="accordion-button ${getCollapsed(
+            "sub-3"
+          )}" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+            Menentukan Persamaan dari Suatu Konfigurasi Objek
           </button>
         </h2>
-        <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-          <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+        <div id="flush-collapseThree" class="accordion-collapse collapse ${getChapClass(
+          "sub-3"
+        )}" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+          <div class="accordion-body">
+            <div class="list-course mb-3">
+              <a href="/materi/sub-3/0/" class="list-course-child ${getChapChild(
+                "sub-3/0/"
+              )}">Pendahuluan</a>
+            </div>
+            <div class="list-course mb-3">
+              <a href="/materi/sub-3/1/" class="list-course-child ${getChapChild(
+                "sub-3/1/"
+              )}">Pola Bilangan Persegi</a>
+            </div>
+            <div class="list-course mb-3">
+              <a href="/materi/sub-3/2/" class="list-course-child ${getChapChild(
+                "sub-3/2/"
+              )}">Pola Bilangan Persegi Panjang</a>
+            </div>
+            <div class="list-course mb-3">
+              <a href="/materi/sub-3/3/" class="list-course-child ${getChapChild(
+                "sub-3/3/"
+              )}">Pola Bilangan Segitiga</a>
+            </div>
+            <div class="list-course mb-3">
+              <a href="/materi/sub-3/4/" class="list-course-child ${getChapChild(
+                "sub-3/4/"
+              )}">Pola Konfigurasi Objek Lainnya</a>
+            </div>
+            <div class="list-course mb-3">
+              <a href="/materi/sub-3/5/" class="list-course-child ${getChapChild(
+                "sub-3/5/"
+              )}">Kuis</a>
+            </div>
+          </div>
         </div>
       </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="flush-headingFour">
+          <button class="accordion-button ${getCollapsed(
+            "sub-4"
+          )}" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+            Latihan
+          </button>
+        </h2>
+        <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+          <div class="accordion-body">
+            <div class="list-course mb-3">
+              <a href="/materi/sub-4/0/" class="list-course-child">Latihan</a>
+            </div>
+          </div>
+        </div>
       </div>
-      <!-- End List course -->
     </div>
+    <!-- End List course -->
     <div class="modal-footer">
       <button type="button" class="btn btn-secondary" id="next" data-bs-dismiss="modal">Tutup</button>
     </div>
   </div>
 </div>
-`
+`;
---
+++ guru/components/sidebar.js
@@ -0,0 +1,33 @@
+const getChapChild = (path) => {
+  return this.location.pathname.substring() === path ? "active" : "";
+};
+
+document.querySelector("#nav").innerHTML = `
+<div>
+  <a href="#" class="nav_logo">
+    <i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name">Guru</span>
+  </a>
+  <div class="nav_list">
+    <a href="home.html" class="nav_link ${getChapChild(
+      "/guru/pages/home.html"
+    )}">
+      <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Dashboard</span>
+    </a>
+    <a href="nilai.html" class="nav_link ${getChapChild(
+      "/guru/pages/nilai.html"
+    )}">
+      <i class='bx bx-user nav_icon'></i> <span class="nav_name">Daftar Nilai</span>
+    </a>
+    <a href="#" class="nav_link">
+      <i class='bx bx-message-square-detail nav_icon'></i> <span class="nav_name">Messages</span>
+    </a>
+  </div>
+</div>
+<a href="#" class="nav_link" id="logout"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">SignOut</span> </a>
+`;
+
+document.getElementById("logout").addEventListener("click", () => {
+  sessionStorage.removeItem("user");
+  sessionStorage.removeItem("pass");
+  window.location = "../";
+});
